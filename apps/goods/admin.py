38677aa18ccac84f43a78b23e1e1838b9d6188c4
ouusikinn <wangzhixinlxy@163.cm>
2020-07-15 14:15:14 +0900

    页面静态化

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from goods.models import GoodsType
 
 # Register your models here.
+admin.site.register(GoodsType)
+# admin.site.register(GoodsType, GoodsTypeAdmin)
+# admin.site.register(GoodsSKU)
+# admin.site.register(Goods)
+# admin.site.register(IndexGoodsBanner, IndexGoodsBannerAdmin)
+# admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmini)
+# admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -5,6 +5,10 @@ from celery import Celery
 # 创建一个Celery的实例
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template import loader, RequestContext
+import os
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from django_redis import get_redis_connection
 
 app = Celery('celery_task.task', broker='redis://192.168.119.128:6379/8')
 
@@ -23,3 +27,45 @@ def send_register_active_email(to_email, username, token):
 
     send_mail(subject, message, sender, receiver, html_message=html_message)
     time.sleep(5)
+
+
+@app.task
+def generate_static_index_html():
+    # 获取商品的种类信息
+    types = GoodsType.objects.all()
+    # 获取首页轮播信
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    # 获取首页促销活动信息
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # 获取首页分类商品展示信息
+    for type in types:
+        # 获取type种类首页分类商品的图片展示信息
+        # 获取type种类首页分类商品的文字展示信息
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+        # 动态给type类型增加属性，分别保存首页分类商品的图片和信息文字
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+    # 组织模板上下文
+    context = {
+        'types': types,
+        'goods_banners': goods_banners,
+        'promotion_banners': promotion_banners,
+
+    }
+    # 使用模板
+    # 1.加载模板文件,返回模板对象
+    temp = loader.get_template('static_index.html')
+    # 2.定义模板上下文
+    # context = RequestContext(request, context)
+    # 3.模板渲染
+    static_index_html = temp.render(context)
+
+    # 生成首页对应的静态文件
+    save_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+
+    with open(save_path, 'w') as f:
+        f.write(static_index_html)
